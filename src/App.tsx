8032e873c00de08037b74cde914b15cc26506835
Eric Heydenberk <eric@quotapath.com>
2025-04-27 15:25:03 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
